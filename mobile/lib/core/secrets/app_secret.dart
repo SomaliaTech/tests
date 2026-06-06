5015365f05ab1d7b81fdff1f41bdebc05fb86651
Hussein mursal <kuneen2@gmail.com>
2026-06-06 18:43:36 +0300

    removed

--- mobile/lib/core/secrets/app_secret.dart
+++ mobile/lib/core/secrets/app_secret.dart
@@ -1,5 +1,1 @@
-class AppSecrets {
-  static const String supabaseUrl = "https://qzhgavvfsqsqerbedvoi.supabase.co";
-  static const publishableKey =
-      "sb_publishable_rtBgqpRmCrxc1-D0QZECdw_qHJ3giQ4";
-}
+class AppSecrets {}
